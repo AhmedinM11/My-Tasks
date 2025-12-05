9e80cfd3e72589d62ed85e9b14a1b7afa4a27478
AhmedinM11 <ahmedinmuhamedd@gmail.com>
2025-12-05 21:51:57 +0300

    Update tasks.dart

--- tasks.dart
+++ tasks.dart
@@ -10,6 +10,8 @@ class Student {
   }
   
 }
+
+
 //for exercise (2)
 class Product {
   final String id;
@@ -17,4 +19,77 @@ class Product {
   double price;
   
   Product(this.name, this.price) : id = "P-001";
+}
+class Device {
+  void turnOn() {
+    print("hi");
+  }
+}
+
+class Phone extends Device {
+  void turnOn() {
+    print("hello");
+  }
+}
+
+class Laptop extends Device {
+  void turnOn() {
+    print("Heyy");
+  }
+}
+
+
+
+//for exercise 2
+class Person {
+  String name;
+  int age;
+  Person(this.name, this.age);
+  void introduce() {
+    
+  }
+}
+class Student extends Person {
+  Student(String name, int age) : super(name, age);  
+  void introduce() {
+  }
+}
+//for the next exercise 
+Future<String> fetchUser() async {
+  await Future.delayed(Duration(seconds: 2),
+    return "User: Alice",);
+}
+Future<String> fetchOrders() async {
+  await Future.delayed(Duration(seconds: 2),
+    return "Orders: 3 Items",);
+}
+
+
+
+//The last one
+class InvAmoExce implements Exception {
+  final String message;
+  InvAmoExce(this.message);
+
+  String toString() return "InvalidAmountException: $message";
+}
+void transferMoney(double amount) {
+  if (amount <= 0) {
+    throw InvAmoExce("Amount must be greater than 0");
+  } else if (amount > 5000) {
+    throw FormatException("Amount exceeds maximum transfer limit of 5000");
+  } else {
+    print("Transfer successful for amount: \$${amount.toStringAsFixed(2)}");
+  }
+}
+void main() {
+  try {
+    transferMoney(2500);
+  } on InvAmoExce catch (e) {
+    print(e);
+  } on FormatException catch (e) {
+    print("FormatException: ${e.message}");
+  } finally {
+    print("Transaction check completed");
+  }
 }
